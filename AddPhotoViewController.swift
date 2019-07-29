67e987fabefa2606be5dcb67b58ce22462fab6b6
Apple <apple@Madj.local>
2019-07-29 16:37:45 -0400

    viewFinder

---
+++ AddPhotoViewController.swift
@@ -0,0 +1,91 @@
+//
+//  AddPhotoViewController.swift
+//  viewFinder
+//
+//  Created by Apple on 7/29/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+
+    var imagePicker = UIImagePickerController()
+
+    @IBOutlet weak var newImage: UIImageView!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        imagePicker.delegate = self
+    }
+    
+    
+    func getPhotos() {
+        
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            
+            
+            if let coreDataPhotos = try? context.fetch(Photos.fetchRequest()) as? [Photos] {
+                photos = coreDataPhotos
+                tableView.reloadData ()
+                
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getPhotos()
+    }
+    
+
+    @IBAction func takePhotoTapped(_ sender: Any) {
+    imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+}
+    
+    @IBAction func photoLibraryTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func photoAlbumTapped(_ sender: Any) {
+        imagePicker.sourceType = .savedPhotosAlbum
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+
+    @IBAction func savePhotoTapped(_ sender: UIButton) {
+        
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            
+            let photoToSave = Photos(entity: Photos.entity(), insertInto: context)
+            photoToSave.caption = textField.text
+            
+            if let userImage = newImage.image {
+                if let userImageData = userImage.pngData() {
+                    photoToSave.imageData = userImageData
+                    
+                }
+                
+            }
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            
+            newImage.image = selectedImage
+            
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+}
